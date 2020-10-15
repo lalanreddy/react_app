37421a4578ba8d5053934062c1cdfdb7cd6dbc08
lalanreddy <lalanreddy1605@gmail.com>
2020-10-15 11:06:54 +0530

    Updates

---
+++ precache-manifest.0e6bf38413482573dbafaa3671af4d66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9d449d14ea5dff074a0e8ac58c213e",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "6f279f73c45e9773f065",
+    "url": "/react_app/static/css/main.8aa23698.chunk.css"
+  },
+  {
+    "revision": "df9d801713574ac67f42",
+    "url": "/react_app/static/js/2.6638c791.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_app/static/js/2.6638c791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f279f73c45e9773f065",
+    "url": "/react_app/static/js/main.3d3c58dd.chunk.js"
+  },
+  {
+    "revision": "aacdce4d658e3e7b356a",
+    "url": "/react_app/static/js/runtime-main.0004d783.js"
+  }
+]);
